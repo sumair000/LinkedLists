5918ecce966e233883ab8a0884643df7e7491164
sumair000 <sumairkhan0091@gmail.com>
2024-05-27 18:13:44 +0500

    added a new node to head

--- singlyLL.cpp
+++ singlyLL.cpp
@@ -12,8 +12,20 @@ class Node{
     }
 };
 
+void insertAtHead(Node* &head, int data){
+
+    // create a new node.
+    Node* newnode = new Node(data);
+
+    newnode->next = head;
+    head = newnode;
+}
+
+
 int main(){
     Node* node1 = new Node(5);
+
+    Node* head = node1;
 
     cout<<node1->data;
 
