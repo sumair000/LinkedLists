7d96b532603f6ebce9930a2e25bf9535d7a92301
sumair000 <sumairkhan0091@gmail.com>
2024-05-27 23:32:32 +0500

    added a new node at the given position of linked list

--- singlyLL.cpp
+++ singlyLL.cpp
@@ -31,6 +31,40 @@ void insertAtTail(Node* &tail,int data){
     
 }
 
+void insertAtPosition(Node* &head,Node* &tail,int data,int position){
+
+    // if the given is position is 1.
+    if (position == 1)
+    {
+        insertAtHead(head,data);
+        return;
+    }
+    
+
+    Node* temp = head;
+    int count = 1;
+    while (count < position -1)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    // if temp-> next is null it means we are at last position
+    if (temp->next == nullptr)
+    {
+        insertAtTail(tail,data);
+        return;
+    }
+    
+
+    //create a new node which have to insert
+    Node* newnode = new Node(data);
+
+    newnode->next = temp->next;
+    temp->next = newnode;
+    
+}
+
 void print(Node* &head){
 
     Node* temp = head;
@@ -52,9 +86,11 @@ int main(){
     Node* tail = node1;
 
     print(head);
-    insertAtTail(tail,7);
+    insertAtHead(head,7);
     print(head);
     insertAtTail(tail,9);
+    print(head);
+    insertAtPosition(head,tail,8,1);
     print(head);
     // cout<<node1->data;
 
